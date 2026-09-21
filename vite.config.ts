ba501c463d85d2a32f885842d6aabe6297e72dc3
Tommaso <hc9rgrckjh@privaterelay.appleid.com>
2026-09-21 23:30:24 +0200

    Remove package deduplication from Vite config
    Removed deduplication of specified packages in Vite config.

--- vite.config.ts
+++ vite.config.ts
@@ -16,6 +16,4 @@ export default defineConfig(() => ({
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
-    dedupe: ["react", "react-dom", "react/jsx-runtime", "react/jsx-dev-runtime", "@tanstack/react-query", "@tanstack/query-core"],
-  },
 }));
